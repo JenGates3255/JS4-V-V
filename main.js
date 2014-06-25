5999ef82430744e07abc5ffdbddf338f42374dfa
JenGates3255 <Jengates3255@gmail.com>
2014-06-25 15:11:37 -0600

    middle

---
+++ main.js
@@ -0,0 +1,75 @@
+// var victimnumber = prompt("How many disater victims do you wish to enter?");
+
+// var vicnam =[]
+// var vicnum =[]
+// var vicstreet=[]
+
+// for (var i=0; i<victimnumber; i++){
+// 	vicnam.push(prompt("victim name"));		
+// 	vicnum.push(prompt("victim number"));
+// 	vicstreet.push(prompt("victim street"));
+// 		console.log(vicnam);
+// 		console.log(vicnum);
+// 		console.log(vicstreet);
+// }
+ 
+// var volunteers = prompt("How many volunteers do you wish to enter?")
+//     var volname = []
+//     var volnum = []
+//     var volstreet= []
+
+//    for (var i=0; i<volunteers; i++){
+//    	volname.push(prompt("volunteer name"));
+//    	volnum.push(prompt("volunteer number"));
+//    	volstreet.push(prompt("volunteer street"));
+//    		console.log(volname);
+//    		console.log(volnum);
+//    		console.log(volstreet);
+//    }
+// alert("victims= " + victimnumber + " volunteers= " + volunteers + " names: " + vicnam + volname);
+
+
+
+
+
+var value = true
+while (value){
+			var vicnam =[]
+ 			var vicnum =[]
+			var vicstreet=[]
+	vicnam.push(prompt("victim name"));		
+	vicnum.push(prompt("victim number"));
+	vicstreet.push(prompt("victim street"));
+		console.log(vicnam);
+		console.log(vicnum);
+		console.log(vicstreet);
+
+value = confirm("Do you want to enter another?")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
