ece238fbfb3a34e5fa34510e32eef4f648c95857
JenGates3255 <Jengates3255@gmail.com>
2014-06-26 08:57:00 -0600

    bonus2

--- main.js
+++ main.js
@@ -32,11 +32,11 @@
 
 
 
-var value = true
+var value = true;
+var vicnam =[];
+var vicnum =[];
+var vicstreet=[];
 while (value){
-			var vicnam =[]
- 			var vicnum =[]
-			var vicstreet=[]
 	vicnam.push(prompt("victim name"));		
 	vicnum.push(prompt("victim number"));
 	vicstreet.push(prompt("victim street"));
@@ -44,16 +44,16 @@ while (value){
 		console.log(vicnum);
 		console.log(vicstreet);
 
-value = confirm("Do you want to enter another?")
+value = confirm("Do you want to enter another?");
 }
 
 
 
 var volunteers = true
+var volnam =[]
+var volnum =[]
+var volstreet=[]
 while (volunteers){
-			var volnam =[]
- 			var volnum =[]
-			var volstreet=[]
 	volnam.push(prompt("volunteer name"));		
 	volnum.push(prompt("volunteer number"));
 	volstreet.push(prompt("volunteer street"));
@@ -65,6 +65,32 @@ volunteers = confirm("Do you want to enter another?")
 }
 
 
+// victim and volunteer matching streets 
+//volstreet = vicstreet
+var in_need = prompt('Name a person in need');
+
+var street;
+// Find the victims street
+
+// For each victim
+for (var i = 0; i < vicnam.length; i++) {
+	// If they are the person in need
+	if (vicnam[i] == in_need) {
+		// Save their street
+        street = vicstreet[i];
+	}
+}
+
+var volsOnStreet = []
+// For each volunteer
+for (var i = 0; i < volnam.length; i++) {
+	// If they are on the same street
+	if (street == volstreet[i]) {
+		volsOnStreet.push(volnam[i]);
+	}
+}
+
+alert('Volunteers on your street: ' + volsOnStreet.join(', '));
 
 
 
